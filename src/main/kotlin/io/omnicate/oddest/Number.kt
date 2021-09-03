397c8df25908cd81913f0e59e86cd51d160136b3
otron <oddweb42+gith@gmail.com>
2021-09-03 11:24:09 +0200

    add javadoc, use rem instead of mod

--- src/main/kotlin/io/omnicate/oddest/Number.kt
+++ src/main/kotlin/io/omnicate/oddest/Number.kt
@@ -1,9 +1,15 @@
 package io.omnicate.oddest
 
+/**
+ * Class containing functions to determine whether a given number is odd.
+ */
 class Oddest {
    companion object {
+       /**
+        * Returns true if num is odd, false if otherwise.
+        */
        fun isOdd(num: Int): Boolean {
-           return num.mod(2) == 1
+           return num.rem(2) == 1
        }
    }
 }
